2fa64a66bfdbbfa247f1b9d60f80c74c10b019fa
AntonHran <anton.hranovskyi@gmail.com>
2023-06-28 00:05:23 +0300

    logging was added, function 'greeting_cleaner' was added, function 'clean_folder_main' was changed

--- DEVELOPMENT/cleaner_functions.py
+++ DEVELOPMENT/cleaner_functions.py
@@ -10,17 +10,21 @@ import unicodedata
 from cleaner_consts import table
 from typing import Tuple, Callable
 from classes import TerminalView
+from logger import get_logging
+
+logger_ = get_logging(__name__)
 
 cleaner_commands = TerminalView()
-cleaner_commands_comm = '''\n\tNow you are in the cleaning module. In this module I help you to sort all files 
-    in the chosen directory thus cleaning your folder.
-    To do that, type the path to your folder according the pattern: <DISC:\\Folder\\Other folder...>
-    To back to main menu, type: <back>'''
+cleaner_commands_comm = '''
+    To sort files, type the path to your folder according the pattern: <DISC:\\Folder\\Other folder...>
+    To back to main menu, type: <back>
+    To see instructions of this module, type: <help>'''
 cleaner_commands.add_command(cleaner_commands_comm)
 root = ''
 
 
 async def process_directory(directory) -> None:
+    logger_.info('Function process_directory')
     async for path in AsyncPath(directory).iterdir():
         if await path.is_file():
             await process_file(directory, path.name)
@@ -36,6 +40,7 @@ async def process_directory(directory) -> None:
 
 
 async def process_file(file_path, file) -> None:
+    logger_.info('Function process_file')
     name, extension = file.rsplit('.', 1)
     func_: tuple = handle_func(extension)
     await func_[0](file_path, f'{root}\\{func_[1]}', name, extension)
@@ -44,6 +49,7 @@ async def process_file(file_path, file) -> None:
 
 
 async def move_to(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info('Function move_to')
     new_name: str = rename(file_name)
     await aiofiles.os.rename(f'{old_path}\\{file_name}.{ext}', f'{old_path}\\{new_name}.{ext}')
     i: int = 0
@@ -59,6 +65,7 @@ async def move_to(old_path: str, new_path: str, file_name: str, ext: str) -> Non
 
 
 async def move_to_archive(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info('Function move_to_archive')
     new_name: str = rename(file_name)
     try:
         await aiofiles.os.makedirs('\\'.join((new_path, new_name.title())))
@@ -69,12 +76,14 @@ async def move_to_archive(old_path: str, new_path: str, file_name: str, ext: str
 
 
 async def move_to_other(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info('Function move_to_other')
     new_name: str = rename(file_name)
     await aiofiles.os.rename(f'{old_path}\\{file_name}.{ext}', f'{old_path}\\{new_name}.{ext}')
     await aiofiles.os.replace(f'{old_path}\\{file_name}.{ext}', f'{new_path}\\{new_name}.{ext}')
 
 
 def rename(file_name: str) -> str:
+    logger_.info('Function rename')
     pattern = r'[a-zA-Z0-9\.\-\(\)]'
     for char in file_name:
         if not re.match(pattern, char):
@@ -86,6 +95,7 @@ def rename(file_name: str) -> str:
 
 
 async def check_folder(old_path: str) -> None:
+    logger_.info('Function check_folder')
     if not os.listdir(old_path):
         await aiofiles.os.rmdir(old_path)
 
@@ -99,6 +109,7 @@ extensions = dict(images=[('jpeg', 'png', 'jpg', 'svg'), move_to],
 
 
 def handle_func(file_extension: str) -> Tuple[Callable, str]:
+    logger_.info('Function handle_func')
     for category, extension in extensions.items():
         if file_extension in extension[0]:
             return extension[1], category.title()
@@ -106,12 +117,14 @@ def handle_func(file_extension: str) -> Tuple[Callable, str]:
 
 
 async def after_check(path: str) -> None:
+    logger_.info('Sorting was finished. After sorting checking is statrted')
     for folder in os.listdir(path):
         if get_folder_size(f'{path}\\{folder}') == 0:
             await aioshutil.rmtree(f'{path}\\{folder}')
 
 
 def get_folder_size(folder_path: str) -> int:
+    logger_.info('getting folder size')
     total_size: int = 0
     for roots, dirs, files in os.walk(folder_path):
         for file in files:
@@ -121,27 +134,41 @@ def get_folder_size(folder_path: str) -> int:
 
 
 async def make_directories(path: str) -> None:
+    logger_.info('Function make_directories')
     if await aiofiles.os.path.exists(path):
         for key in extensions:
             await aiofiles.os.makedirs('\\'.join((path, key.title())), exist_ok=True)
         await aiofiles.os.makedirs('\\'.join((path, 'Other')), exist_ok=True)
     else:
+        logger_.error('Error with an entered path')
         print('Something went wrong. Check a validity of the entered path.')
 
 
 def instructions() -> None:
+    logger_.info('Function instructions')
     for command in cleaner_commands.display_commands():
         print(command)
 
 
+def greeting_cleaner() -> None:
+    logger_.info('Function greeting_cleaner')
+    print('''\n\tNow you are in the cleaning module. In this module I help you to sort all files 
+    in the chosen directory thus cleaning your folder.''')
+
+
 async def clean_folder_main() -> None:
+    logger_.info('Function clean_folder_main')
+    greeting_cleaner()
     instructions()
     global root
     while True:
-        root = input('\nType a path to a folder to clean: ')
+        root = input('\nType a path to a folder to clean or a command: ')
         if root == 'back':
             print('\nYou returned to the main Menu.')
             break
-        await make_directories(root)
-        await process_directory(root)
-        await after_check(root)
+        elif root == 'help':
+            instructions()
+        else:
+            await make_directories(root)
+            await process_directory(root)
+            await after_check(root)
